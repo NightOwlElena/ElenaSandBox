8830e25dc4f4aaf5da4519ef34b9f56ce7b2eb57
elenazzz2812 <elenalee2812@gmail.com>
2018-04-20 20:06:35 +0800

    updating the destination

--- extensionscript.sh
+++ extensionscript.sh
@@ -9,8 +9,8 @@ apt-get -y install apache2 php
 apt-get -y install apache2 php
 
 cd /var/www/html/
-wget https://raw.githubusercontent.com/elenazzz2812/ElenaSandBox/master/example.php /var/www/html/
-wget https://raw.githubusercontent.com/elenazzz2812/ElenaSandBox/master/index.php /var/www/html/
+wget https://raw.githubusercontent.com/elenazzz2812/ElenaSandBox/master/example.php 
+wget https://raw.githubusercontent.com/elenazzz2812/ElenaSandBox/master/index.php 
 rm index.html
 # restart Apache
 apachectl restart
