5225d6131773a9992dab06f4f1d73d97ce0571fe
elenazzz2812 <elenalee2812@gmail.com>
2018-04-20 19:08:55 +0800

    Testing out template

---
+++ extensionscript.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# install Apache and PHP (in a loop because a lot of installs happen
+# on VM init, so won't be able to grab the dpkg lock immediately)
+sudo su -
+apt-get -y update
+apt-get -y update
+apt-get -y install apache2 php
+apt-get -y install apache2 php
+
+cp 'https://elenaobjecttest.blob.core.windows.net/test/TemplateDemo/index.php' /var/www/html/
+cp 'https://elenaobjecttest.blob.core.windows.net/test/TemplateDemo/example.php' /var/www/html/
+rm /var/www/html/index.html
+# restart Apache
+apachectl restart
+
